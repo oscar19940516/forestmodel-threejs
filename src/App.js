6448cbc7ea5af639f745933dc00f830f11933825
oscar19940516 <136386572+oscar19940516@users.noreply.github.com>
2023-06-14 00:28:17 +0900

    overlay remove

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ export default function App() {
         <OrbitControls autoRotate maxPolarAngle={Math.PI / 2.18} autoRotateSpeed={0.3} maxDistance={33} minDistance={8} />
       </Canvas>
       <Loader />
-      /*<Overlay /> */
     </>
   )
 }
